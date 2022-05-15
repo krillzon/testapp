5217b5a3aa033b60ced324fed7da5dbad9c49301
Christoffer Stathin <kribbe123@hotmail.se>
2022-05-15 14:02:50 +0200

    Add bottom navbar

--- myfirstapp/lib/main.dart
+++ myfirstapp/lib/main.dart
@@ -1,27 +1,57 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(CupertinoApp(
+                      home: HomeScreen(),
+                      localizationsDelegates: 
+                      const <LocalizationsDelegate<dynamic>>[
+                      DefaultMaterialLocalizations.delegate,
+                      DefaultWidgetsLocalizations.delegate]));
 
-class MyApp extends StatefulWidget {
-  
+final GlobalKey<NavigatorState> firstTabNavKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> secondTabNavKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> thirdTabNavKey = GlobalKey<NavigatorState>();
+
+class HomeScreen extends StatefulWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-
-  void answerQuestion() {
-    setState(() {
-      questionIndex += 1;
-    });
-    print(questionIndex);
-  }
-
+// Tabs
+class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Container(
+      color: Colors.redAccent,
+    );
+  }
+}
+
+class MapTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.greenAccent,
+    );
+  }
+}
+
+class FriendsTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
+
+// Home screen class
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    /*
     var questions = ["whats your favorite animal?",
                      "whats your favorite color?"];
     return MaterialApp(
@@ -39,5 +69,41 @@ class _MyAppState extends State<MyApp> {
             ],
           ))),
     );
+  }
+  */
+  return CupertinoTabScaffold(
+    tabBar: CupertinoTabBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          label: 'Friends',
+        ),
+      ],
+    ), tabBuilder: (BuildContext context, int index) {
+        if (index == 0) {
+          return CupertinoTabView(
+            navigatorKey: firstTabNavKey,
+            builder: (BuildContext context) => HomeTab(),
+          );
+        } else if (index == 1) {
+          return CupertinoTabView(
+            navigatorKey: secondTabNavKey,
+            builder: (BuildContext context) => MapTab(),
+          );
+        } else {
+          return CupertinoTabView(
+            navigatorKey: thirdTabNavKey,
+            builder: (BuildContext context) => FriendsTab(),
+          );
+        } 
+      });
   }
 }
